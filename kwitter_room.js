9f16979430150b37bdf3f3ba50e3291aaa52177f
aarnas2909 <86672300+aarnas2909@users.noreply.github.com>
2021-12-19 12:34:35 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAGVHsfynn2eFcv0onQvB0vux7FHiJXZpU",
+    authDomain: "lets-chat-app-314be.firebaseapp.com",
+    databaseURL: "https://lets-chat-app-314be-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-app-314be",
+    storageBucket: "lets-chat-app-314be.appspot.com",
+    messagingSenderId: "732657315876",
+    appId: "1:732657315876:web:3b4ed534a2dc568943a8b1"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
